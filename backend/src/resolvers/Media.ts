a1ca9a2e13167f99459ee88e541dd3b3a304eaa0
jpswt <joel_swt@yahoo.com>
2023-08-05 18:13:29 -0500

    likes working correctly

--- backend/src/resolvers/Media.ts
+++ backend/src/resolvers/Media.ts
@@ -2,6 +2,7 @@ import { Context } from '..';
 import { userLoader } from '../utils/userLoader';
 interface MediaParent {
 	musicianId: number;
+	id: string;
 }
 
 // Before n+1 data loader
@@ -18,5 +19,12 @@ interface MediaParent {
 export const Media = {
 	user: (parent: MediaParent, args: any, { prisma }: Context) => {
 		return userLoader.load(parent.musicianId);
+	},
+	likesCount: async (parent: MediaParent, args: any, { prisma }: Context) => {
+		return prisma.like.count({
+			where: {
+				mediaId: Number(parent.id),
+			},
+		});
 	},
 };
