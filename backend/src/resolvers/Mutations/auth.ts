8098c1d9660fec8130f100c6c335457b95155df0
jpswt <joel_swt@yahoo.com>
2023-08-09 14:36:31 -0500

    added profile to register

--- backend/src/resolvers/Mutations/auth.ts
+++ backend/src/resolvers/Mutations/auth.ts
@@ -9,6 +9,8 @@ interface RegisterArgs {
 		password: string;
 	};
 	name: string;
+	bio: string;
+	image: string;
 }
 
 interface LoginArgs {
@@ -28,7 +30,7 @@ interface UserPayloadType {
 export const authResolvers = {
 	userRegister: async (
 		parent: any,
-		{ credentials, name }: RegisterArgs,
+		{ credentials, name, bio, image }: RegisterArgs,
 		{ prisma }: Context
 	): Promise<UserPayloadType> => {
 		const { email, password } = credentials;
@@ -95,6 +97,13 @@ export const authResolvers = {
 				name,
 				email,
 				password: hash,
+			},
+		});
+		await prisma.profile.create({
+			data: {
+				bio,
+				image,
+				userId: user.id,
 			},
 		});
 		// sign JWT token
